a42a9e2857b8761e51711005aa5a25239d6878cd
Youthtrouble <harjhibolar@gmail.com>
2021-07-19 11:54:58 +0100

    added getPairs

--- example/example.go
+++ example/example.go
@@ -24,38 +24,45 @@ func main() {
 
 	authorize := orderbooks.Buycoins(os.Getenv("PUBLIC_KEY"), os.Getenv("SECRET_KEY"))
 
-	getOrders, err := authorize.GetOrders("btc_ngnt", "pending", "buy")
+	getPairs, err := authorize.GetPairs()
 	if err != nil {
 		log.Println(err)
 	}
 
-	fmt.Printf("%+v\n", getOrders)
+	fmt.Printf("%s\n", getPairs)
 
-	cancelOrder, err := authorize.CancelOrder("UHJvT3JkZXItZDRmYjMyZDYtOGZjMy00ZTJlLWEzNGYtNTQ3YmEwMzcxMDQ0")
-	if err != nil {
-		log.Println(err)
-	}
+	// getOrders, err := authorize.GetOrders("btc_ngnt", "pending", "buy")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	fmt.Printf("%+v\n", cancelOrder)
+	// fmt.Printf("%+v\n", getOrders)
 
-	getProOrderFee,err := authorize.GetProOrderFees("market_order", "btc_usdt", "sell", 0.001)
-	if err != nil {
-		log.Println(err)
-	}
+	// cancelOrder, err := authorize.CancelOrder("UHJvT3JkZXItZDRmYjMyZDYtOGZjMy00ZTJlLWEzNGYtNTQ3YmEwMzcxMDQ0")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	fmt.Printf("%+v\n", getProOrderFee)
+	// fmt.Printf("%+v\n", cancelOrder)
 
-	postProMarketOrder, err := authorize.PostProMarketOrder("btc_usdt", 5.00, "buy")
-	if err != nil {
-		log.Println(err)
-	}
+	// getProOrderFee,err := authorize.GetProOrderFees("market_order", "btc_usdt", "sell", 0.001)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	fmt.Printf("%+v\n", postProMarketOrder)
+	// fmt.Printf("%+v\n", getProOrderFee)
 
-	postProLimitOrder, err := authorize.PostProLimitOrder("btc_ngnt", 0.005, 2000000.00, "buy", "good_til_cancelled")
-	if err != nil {
-		log.Println(err)
-	}
+	// postProMarketOrder, err := authorize.PostProMarketOrder("btc_usdt", 5.00, "buy")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	fmt.Printf("%+v\n", postProLimitOrder)
+	// fmt.Printf("%+v\n", postProMarketOrder)
+
+	// postProLimitOrder, err := authorize.PostProLimitOrder("btc_ngnt", 0.005, 2000000.00, "buy", "good_til_cancelled")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+
+	// fmt.Printf("%+v\n", postProLimitOrder)
 }
